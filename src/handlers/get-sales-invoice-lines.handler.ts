f816379a281bdf0d016da7779b6c82d13995b5c1
AnmolWenodo <anmolwork98@gmail.com>
2026-05-06 21:11:59 +0530

    tool fixed

--- src/handlers/get-sales-invoice-lines.handler.ts
+++ src/handlers/get-sales-invoice-lines.handler.ts
@@ -5,30 +5,28 @@ export async function getSalesInvoiceLinesHandler(input: any) {
     const db = getDb();
     console.log("Sales Lines Tool Called");
     console.log("Input Parameters:", input);
-     let branchId: string | null = null;
-     if (input.branchId !== undefined && input.branchId !== null) {
-      if (Array.isArray(input.branchId)) {
-        // e.g. [1,2,3] → "1,2,3"
-        branchId = input.branchId.join(",");
-      } else {
-        // single value → "1"
-        branchId = String(input.branchId);
-      }
-    }
+     let branchIds: string | null = null;
+    
+  branchIds = Array.isArray(input.branchIds)
+      ? input.branchIds.join(",")   // e.g. [1,2,3] → "1,2,3"
+      : input.branchIds !== undefined && input.branchIds !== null
+        ? String(input.branchIds)   // single value → "1"
+        : null;                    // undefined or null → null
   const groupBy = Array.isArray(input.groupBy)
     ? input.groupBy.join(",")   // → "1,3"
     : String(input.groupBy); 
+
 
     const result = await db
       .request()
       .input("PI_START_DATE", input.fromDate)
       .input("PI_END_DATE", input.toDate)
       .input("PI_ENTITY_ID", input.entityId ?? 0)
-      .input("PI_BRANCH_ID", branchId ?? 0)
+      .input("PI_BRANCH_ID", branchIds)
       .input("PI_CUSTOMER_ID", input.customerId ?? 0)
       .input("PI_GROUP_BY", groupBy ?? '') 
       .execute("PRC_GET_PRODUCT_AND_CATEGORY_WISE_SALES_SUMMARY");
-    console.log("Raw DB Result1:", result.recordset);
+    console.log("Raw DB Result1:", result);
     return {
       result: result.recordset ?? [],
       isError: false,
